01e32536c40460dc2e4693aa7745e994b3a3d5fe
Gibson1987R <gibsonrosales@gmail.com>
2023-07-22 15:08:01 -0400

    explicacion inicial de variables con var

---
+++ js/01.js
@@ -0,0 +1,28 @@
+/* Variables 
+Existen 3 tipos 
+
+Las partes de las variables son tres: palabra reservada, nombre y asignacion o valor
+Cuando aparecen las tres partes significa que: se inicializo y se le asigno valor
+
+JS es dinamico y no necesitas indicar que tipo de dato tendra la variable. Las variables mismas guardan el tipo de dato y puede cambiar en momento que deseemos.
+
+Las variables son case sensetive
+*/
+
+var producto = 'Audifonos Gamers' // iniciada y con valor
+
+var nombre //solo iniciada
+
+producto = true // reasignacion de valor y tipo de dato
+
+var mama = 'Anais', // se pueden inicializar multiples sin necesidad de escribir varias veces la palabra reservada
+  hijo = 'Joaquin',
+  padre = 'Samuel'
+
+var nombre_producto = 'Camisa Blanca' // estilo underscore
+var nombreProducto = 'Camisa Blanca' // Camelcase
+var NombreProducto = 'Camisa Blanca' // Pascal Case
+var nombreproducto = 'Camisa Blanca' // lowercase
+
+console.log(nombre)
+/* console.log(PRODUCTO) */
