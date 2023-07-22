4f2d17ad0d68d3276847d35e391ea64ccf4e0fb8
Gibson1987R <gibsonrosales@gmail.com>
2023-07-22 16:02:42 -0400

    Terminadas var let y const

---
+++ js/02.js
@@ -0,0 +1,17 @@
+//Variables con let
+
+let producto = 'Audifonos Gamers' // iniciada y con valor
+
+let nombre //solo iniciada
+
+producto = true // reasignacion de valor y tipo de dato
+
+let mama = 'Anais', // se pueden inicializar multiples sin necesidad de escribir varias veces la palabra reservada
+  hijo = 'Joaquin',
+  padre = 'Samuel'
+
+//estilo
+let nombre_producto = 'Camisa Blanca' // underscore
+let nombreProducto = 'Camisa Blanca' // Camelcase
+let NombreProducto = 'Camisa Blanca' // Pascal Case
+let nombreproducto = 'Camisa Blanca' // lowercase
---
+++ js/03.js
@@ -0,0 +1,17 @@
+//Variables con const: es solo para valores fijos
+
+const producto = 'Audifonos Gamers' // iniciada y con valor
+
+/* const nombre  */ // no se puede, deben ser inicializadas
+
+producto = true // reasignacion de valor y tipo de dato
+
+const mama = 'Anais', // se pueden inicializar multiples sin necesidad de escribir varias veces la palabra reservada
+  hijo = 'Joaquin',
+  padre = 'Samuel'
+
+//estilo
+const nombre_producto = 'Camisa Blanca' // underscore
+const nombreProducto = 'Camisa Blanca' // Camelcase
+const NombreProducto = 'Camisa Blanca' // Pascal Case
+const nombreproducto = 'Camisa Blanca' // lowercase
