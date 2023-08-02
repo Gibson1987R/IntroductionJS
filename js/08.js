b602a35824594bf07b3c3ebbc0988d074691464b
Gibson1987R <gibsonrosales@gmail.com>
2023-08-02 17:44:55 -0400

    concatenacion fin de numero y string

---
+++ js/08.js
@@ -0,0 +1,11 @@
+let separador = '----------------------'
+//Concatenacion
+const nombre = 'Gibson'
+const correo = 'gibsonrosales@gmail.com'
+
+console.log('Nombre: ' + nombre + ' ' + 'Correo: ' + correo)
+console.log(separador)
+
+//Template String
+console.log(`Nombre: ${nombre} Correo: ${correo}`)
+console.log(separador)
