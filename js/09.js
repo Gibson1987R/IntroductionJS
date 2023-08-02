298120742f395a472f01c81b4c600a472ec30149
Gibson1987R <gibsonrosales@gmail.com>
2023-08-02 17:56:01 -0400

    booleams terminado

---
+++ js/09.js
@@ -0,0 +1,12 @@
+//Booleams: solo pueden tener dos valores true o false
+
+const booleam1 = true
+console.log(booleam1)
+const booleam2 = false
+console.log(booleam2)
+const booleam3 = 'true' // fijese que si tiene comillas es string no booleam, los booleams no llevan comillas.
+console.log(booleam3)
+
+//otra forma de crear booleams es con el constructor.
+const booleam4 = new Boolean(true)
+console.log(booleam4)
